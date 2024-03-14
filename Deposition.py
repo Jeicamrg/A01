72e0a70156c4de78e5c7f9c83b44d1429b8f26d0
azdanowicz02 <a.zdanowicz@student.tudelft.nl>
2024-03-14 11:56:00 +0100

    deposition updated

--- Deposition.py
+++ Deposition.py
@@ -8,16 +8,10 @@ pre_biomineralization_weight = df['Pre biomineralization weight (g)'].tolist()
 biomineralized_weight = df['Biomineralized weight (g)'].tolist()
 
 time=[3,9,12,15,18,21,24] #hours as a incubation time
-print(pre_biomineralization_weight)
-print(biomineralized_weight)
 
-
-
-
+plt.figure()
 plt.plot(time, pre_biomineralization_weight, marker='o', label='Pre-biomineralization weight')
 plt.plot(time, biomineralized_weight, marker='o', label='Biomineralized weight')
-
-# Add labels and title
 plt.xlabel('Time (hours)')
 plt.ylabel('Weight (g)')
 plt.title('Weight vs Time')
@@ -28,4 +22,21 @@ plt.legend()
 # Show plot
 plt.grid(True)
 plt.savefig('deposition.png')
+
+difference=[]
+
+for i in range(len(time)):
+    diff = ((biomineralized_weight[i] - pre_biomineralization_weight[i]) / pre_biomineralization_weight[i]) * 100
+    difference.append(diff)
+
+print(difference)
+
+plt.figure()
+plt.plot(time, difference, label='Percentage difference (%)')
+plt.xlabel('Incubation time [hours]')
+plt.ylabel('Percentage difference (%)')
+plt.title('Percentage difference in weight vs incubation time')
+plt.legend()
+plt.grid(True)
+plt.savefig('percentage_difference.png')
 
