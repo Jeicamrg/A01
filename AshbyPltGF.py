083c067df1f39afdcc656d617da174eb294a94c3
adam91101 <elassaladam@gmail.com>
2024-04-29 11:51:29 +0200

    Update AshbyPltGF.py

--- AshbyPltGF.py
+++ AshbyPltGF.py
@@ -1,25 +1,22 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Mar 18 09:13:55 2024
-
-@author: elass
-"""
-
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.patches import Ellipse
 
 # Input data
 Density = [1800, 1712, 1800, 1830, 1643, 1920] #kg/m^3
-E_Modulus = [30.9, 54.5, 39.065, 37.97, 27.97, 15.32] #GPa
-Density_Actual = [3000] #kg/m^3 --> CHANGE
-E_ModActual = [50] #GPa --> CHANGE
+E_Modulus = [30.9, 54.5, 39.065, 37.97, 27.97, 15.32] #GPa, Tensile Modulus
+Biom_Density = [1976.8,1829.1,1912.3, 1696.2] #kg/m^3
+Biom_Emod = [45.655008, 48.05929, 40.14571, 40.03018]#GPa, Tensile Modulus
+Non_Biom_Density = [2500.5, 2436.4, 2505.7, 2407.2] #kg/m^3
+Non_Biom_Emod = [54.89653, 27.59863, 50.42237, 39.23931] #GPa
 
 # Combine the data into an array of points
 points = np.column_stack((Density, E_Modulus))
+Biom_points = np.column_stack((Biom_Density, Biom_Emod))
+Non_Biom_points = np.column_stack((Non_Biom_Density, Non_Biom_Emod))
 
 # Function to draw an ellipse around points
-def draw_ellipse_around_points(points):
+def draw_ellipse_around_points(points, fill_colour, border_colour):
     # Calculate the mean of the points
     center = np.mean(points, axis=0)
     
@@ -29,7 +26,7 @@ def draw_ellipse_around_points(points):
     
     # Width and height of the ellipse are sqrt of eigenvalues (standard deviations)
     # multiplied by a factor for scaling (to ensure the ellipse covers all points)
-    width, height = 2.1 * np.sqrt(eigenvalues) * 2.1  
+    width, height = 2.05 * np.sqrt(eigenvalues) * 2.05 
     
     # Calculate the angle of rotation in degrees
     angle = np.arctan2(*eigenvectors[:,0][::-1])
@@ -37,21 +34,27 @@ def draw_ellipse_around_points(points):
     
     # Create an ellipse
     ellipse = Ellipse(xy=center, width=width, height=height, angle=angle, 
-                      edgecolor='blue', fc='lightblue', alpha=0.5, lw=2)
+                      edgecolor=border_colour, fc= fill_colour, alpha=0.5, lw=2)
     
     return ellipse
 
 # Draw the ellipse
-ellipse = draw_ellipse_around_points(points)
+ellipse = draw_ellipse_around_points(points, border_colour='blue', fill_colour='lightblue')
+Biom_ellipse = draw_ellipse_around_points(Biom_points, border_colour='lime', fill_colour='palegreen')
+Non_Biom_ellipse = draw_ellipse_around_points(Non_Biom_points, border_colour='orange', fill_colour='bisque')
 
 # Plotting
 fig, ax = plt.subplots()
-ax.scatter(Density, E_Modulus, color='black', label='Literature Values')
+ax.scatter(Density, E_Modulus, color='blue', label='Glass Fiber Composites')
 ax.add_patch(ellipse)
-plt.scatter(Density_Actual, E_ModActual, color = 'red', label = 'Actual Value')
-ax.set_xlabel('Density (kg/m^3)')
+ax.scatter(Biom_Density, Biom_Emod, color='green', label='Biomineralized Samples')
+ax.add_patch(Biom_ellipse)
+ax.scatter(Non_Biom_Density, Non_Biom_Emod, color='orange', label='Non-Biomineralized Samples')
+ax.add_patch(Non_Biom_ellipse)
+#plt.scatter(Density_Actual, E_ModActual, color = 'red', label = 'Actual Value')
+ax.set_xlabel('Density (kg/$m^3$)')
 ax.set_ylabel('Elastic Modulus (GPa)')
-ax.set_title('Ashby Plot: Elastic Modulus vs. Density')
+#ax.set_title('Ashby Plot: Elastic Modulus vs. Density')
 ax.legend(loc='lower right')
 plt.grid(True)
 ax.set_aspect('auto', 'datalim')
